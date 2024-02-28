b1e3ce33b6a674bbb2c1e6ded429564e5437c830
Abdul Karim Khan <abdulkarimkhan.cs@hotmail.com>
2024-02-29 02:08:15 +0500

    Add User Interaction Guide for tvOS

---
+++ tvOS-Sample-Guide/Episode 1  - User Interaction/UserInteraction.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  tvOS-Sample-Guide
+//
+//  Created by Abdul Karim Khan on 27/02/2024.
+//
+
+import SwiftUI
+
+struct UserInteraction: View {
+    
+    @State var buttonSelected = ""
+    
+    var body: some View {
+        VStack {
+            Text("Button Pressed \(buttonSelected)")
+                .font(.title)
+                .bold()
+                .padding(20)
+
+            Button {
+                buttonSelected = "Login"
+            } label: {
+                Text("Login")
+            }
+            
+            Button {
+                buttonSelected = "Register"
+            } label: {
+                Text("Register")
+            }
+
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    UserInteraction()
+}
---
+++ tvOS-Sample-Guide/tvOS_Sample_GuideApp.swift
@@ -0,0 +1,17 @@
+//
+//  tvOS_Sample_GuideApp.swift
+//  tvOS-Sample-Guide
+//
+//  Created by Abdul Karim Khan on 27/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct tvOS_Sample_GuideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            UserInteraction()
+        }
+    }
+}
